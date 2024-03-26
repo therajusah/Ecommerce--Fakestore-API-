a406c16c345d5436a6730fd8beeb3ea955ad4ffb
therajusah <workwithrajukumar@gmail.com>
2024-03-26 16:35:02 +0530

    added categories of products from API

--- js/index.js
+++ js/index.js
@@ -12,4 +12,17 @@ return data;
 
 async function populateCategories() {
     const categories = await fetchCategories();
+    const categoryList = document.getElementById("categoryList");
+    categories.forEach(category => {
+        const categoryHolder = document.createElement("div");
+        const categoryLink = document.createElement("a");
+        categoryLink.href = "#";
+        categoryLink.textContent = category; // Setting the category name as the text of the anchor tag
+        categoryHolder.classList.add("category-item", "d-flex", "align-items-center", "justify-content-center")
+        categoryHolder.appendChild(categoryLink);
+
+        categoryList.appendChild(categoryHolder);
+    })
 }
+
+populateCategories();
