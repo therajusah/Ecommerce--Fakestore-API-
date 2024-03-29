60ded88918e877c3b5f415d05f2bc88c02c03c6c
therajusah <workwithrajukumar@gmail.com>
2024-03-29 21:14:38 +0530

    done code optimization to fasten the filtering of products on the basis of price range.

--- js/productList.js
+++ js/productList.js
@@ -1,67 +1,71 @@
-document.addEventListener("DOMContentLoaded", () => {
-    async function fetchProducts() {
-        const response = await axios.get("https://fakestoreapi.com/products");
-        console.log(response.data);
-        return response.data;
+document.addEventListener("DOMContentLoaded", async () => {
+  async function fetchProducts() {
+    const response = await axios.get("https://fakestoreapi.com/products");
+    console.log(response.data);
+    return response.data;
+  }
+
+  const downloadedProducts = await fetchProducts();
+
+  async function populateProducts(flag, customProducst) {
+    let products = customProducst;
+    if (flag == false) {
+      products = await fetchProducts();
     }
-    async function populateProducts(flag, customProducst) {
-        let products = customProducst;
-        if(flag == false){
-             products = await fetchProducts();
-        }
-        const productLists  = document.getElementById("productList");
-        products.forEach(product => {
-            const productItem = document.createElement("a");
-            productItem.target = "_blank";
-            productItem.classList.add("product-item", "text-decoration-none", "d-inline-block")
-            productItem.href = "productDetails.html";
+    const productLists = document.getElementById("productList");
+    products.forEach((product) => {
+      const productItem = document.createElement("a");
+      productItem.target = "_blank";
+      productItem.classList.add(
+        "product-item",
+        "text-decoration-none",
+        "d-inline-block"
+      );
+      productItem.href = "productDetails.html";
 
-            const productImage = document.createElement("div");
-            const productName = document.createElement("div");
-            const productPrice = document.createElement("div");
+      const productImage = document.createElement("div");
+      const productName = document.createElement("div");
+      const productPrice = document.createElement("div");
 
+      productImage.classList.add("product-img");
+      productName.classList.add("product-name", "text-center");
+      productPrice.classList.add("product-price", "text-center");
 
-            productImage.classList.add("product-img");
-            productName.classList.add("product-name", "text-center");
-            productPrice.classList.add("product-price", "text-center");
+      productName.textContent = product.title.substring(0, 12) + "...";
 
-            productName.textContent = product.title.substring(0, 12)+ "...";
+      productPrice.textContent = `&#8377; ${product.price}`;
 
-            productPrice.textContent = `&#8377; ${product.price}`;
+      const imageInsideProductImage = document.createElement("img");
+      imageInsideProductImage.src = product.image;
 
+      // append divs
 
-            const imageInsideProductImage = document.createElement("img");
- imageInsideProductImage.src = product.image;
+      productImage.appendChild(imageInsideProductImage);
+      productItem.appendChild(productImage);
+      productItem.appendChild(productName);
+      productItem.appendChild(productPrice);
 
- // append divs
+      productLists.appendChild(productItem);
+    });
+  }
 
- productImage.appendChild(imageInsideProductImage);
- productItem.appendChild(productImage);
- productItem.appendChild(productName);
- productItem.appendChild(productPrice);
+  populateProducts(false);
 
-productLists.appendChild(productItem);
-        })
-    }
-
-    populateProducts(false);
-
-    const filterSearch = document.getElementById("search");
-    filterSearch.addEventListener("click", async () => {
-        const productList = document.getElementById("productList");
-
+  const filterSearch = document.getElementById("search");
+  filterSearch.addEventListener("click", async () => {
+    const productList = document.getElementById("productList");
     const minPrice = Number(document.getElementById("minPrice").value);
     const maxPrice = Number(document.getElementById("maxPrice").value);
-const products = await fetchProducts();
-filteredProducts = products.filter(product => product.price >= minPrice && product.price <= maxPrice);
-productList.innerHTML ="";
-populateProducts(true, filteredProducts);
+    const products = downloadedProducts;
+    filteredProducts = products.filter(
+      (product) => product.price >= minPrice && product.price <= maxPrice
+    );
+    productList.innerHTML = "";
+    populateProducts(true, filteredProducts);
+  });
 
-    });
-
-
-    const resetFilter = document.getElementById("clear");
-    resetFilter.addEventListener("click", () =>  {
-        window.location.reload();
-    })
-})
+  const resetFilter = document.getElementById("clear");
+  resetFilter.addEventListener("click", () => {
+    window.location.reload();
+  });
+});
