f932398265e41b948aa318334be2f10ebeafd76f
therajusah <workwithrajukumar@gmail.com>
2024-04-01 13:17:56 +0530

    "update cart item rendering with quantity selector and remove button in cart.html and cart.js"

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-   function prepareWrapperDivForCartItems(product) {
-    const orderDetailsProduct = document.createElement("div");
-    orderDetailsProduct.classList.add("order-details-product", "d-flex", "flex-row");
 
+  
+    function prepareWrapperDivForCartItems(product, productQuantityMapping) {
+    const orderDetailsProduct = document.createElement("div");
+    orderDetailsProduct.classList.add(
+      "order-details-product",
+      "d-flex",
+      "flex-row"
+    );
 
     const orderDetailsProductImg = document.createElement("div");
     orderDetailsProductImg.classList.add("order-details-product-img", "d-flex");
@@ -10,23 +15,62 @@ document.addEventListener("DOMContentLoaded", () => {
     image.src = product.image;
     orderDetailsProductImg.appendChild(image);
     const orderDetailsProductData = document.createElement("div");
-    orderDetailsProductData.classList.add("order-details-product-data", "d-flex", "flex-row");
+    orderDetailsProductData.classList.add(
+      "order-details-product-data",
+      "d-flex",
+      "flex-row"
+    );
     const name = document.createElement("div");
     const price = document.createElement("div");
     name.textContent = product.title;
     price.textContent = product.price;
+    orderDetailsProductData.appendChild(name);
+    orderDetailsProductData.appendChild(price);
 
     const orderDetailsProductActions = document.createElement("div");
-    orderDetailsProductActions.classList.add("order-details-product-action", "d-flex", "flex-column");
+    orderDetailsProductActions.classList.add(
+      "order-details-product-action",
+      "d-flex",
+      "flex-column"
+    );
     const orderDetailsProductQuatity = document.createElement("div");
     orderDetailsProductQuatity.classList.add("order-details-product-quantity");
     const quantityLabel = document.createElement("div");
-    quantityLabel.textContent = Quantity;
+    quantityLabel.textContent = "Quantity";
     quantityLabel.classList.add("fw-bold");
-   }
+    const formGroup = document.createElement("div");
+    formGroup.classList.add("form-group");
+    const select = document.createElement("select");
+    select.classList.add("form-select");
+    for (let i = 0; i <= 10; i++) {
+      const option = document.createElement("option");
+      option.textContent = i;
+      option.value = i;
+      if (i == productQuantityMapping[product.id]) {
+        option.selected = "true";
+      }
+      select.appendChild(option);
+    }
+    formGroup.appendChild(select);
+    orderDetailsProductQuatity.appendChild(quantityLabel);
+    orderDetailsProductQuatity.appendChild(formGroup);
+    orderDetailsProductActions.appendChild(orderDetailsProductQuatity);
+    const remove = document.createElement("button");
+    remove.classList.add("order-details-product-remove", "btn", "btn-danger");
+    remove.textContent = "Remove";
+    orderDetailsProductActions.appendChild(remove);
 
-    async function populateCart() {
-    const cart = await fetchCartById(2);
+    orderDetailsProduct.appendChild(orderDetailsProductImg);
+    orderDetailsProduct.appendChild(orderDetailsProductData);
+    orderDetailsProduct.appendChild(orderDetailsProductActions);
+
+    const hr = document.createElement("hr");
+    document.getElementById("orderDetails").appendChild(orderDetailsProduct);
+    document.getElementById("orderDetails").appendChild(hr);
+  }
+
+  async function populateCart() {
+    const cart = await fetchCartById(1);
     const cartProducts = cart.products;
     const productQuantityMapping = {};
 
@@ -38,11 +82,13 @@ document.addEventListener("DOMContentLoaded", () => {
     let totalPrice = 0;
 
     products.forEach((product) => {
+        prepareWrapperDivForCartItems(product, productQuantityMapping);
       totalPrice += product.price;
+
     });
 
     document.getElementById("total-price").textContent = totalPrice;
     document.getElementById("net-price").textContent = totalPrice - 10;
-  }
+}
   populateCart();
 });
